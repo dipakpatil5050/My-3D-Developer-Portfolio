7d289d53e1bb9d4c982c19080d185ebd9ca1aad9
Dipak Patil <92545685+dipakpatil5050@users.noreply.github.com>
2025-05-30 16:28:46 +0530

    fixewd the issue with the input handling

--- src/assets/index.js
+++ src/assets/index.js
@@ -6,7 +6,6 @@ import web from "./web.png";
 import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
-
 import css from "./tech/css.png";
 import docker from "./tech/docker.png";
 import figma from "./tech/figma.png";
@@ -30,7 +29,7 @@ import carrent from "./carrent.png";
 import jobit from "./jobit.png";
 import tripguide from "./tripguide.png";
 import ERP from "./SYSPROERP.png";
-import Deepdocs from "./deepdocs.png"
+import Deepdocs from "./deepdocs.png";
 
 export {
   logo,
