a8d03a7084181f54359d4c40e61a30cdfbdf47df
Dipak Patil <92545685+dipakpatil5050@users.noreply.github.com>
2025-05-30 16:27:04 +0530

    fixx the isssuein index js

--- src/assets/index.js
+++ src/assets/index.js
@@ -29,7 +29,7 @@ import tesla from "./company/tesla.png";
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
 import tripguide from "./tripguide.png";
-import ERP from "./SYSPROERP.png"
+import ERP from "./SYSPROERP.png";
 import Deepdocs from "./deepdocs.png"
 
 export {
